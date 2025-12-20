a768c936005242096068fe40b2a808fa3be5b7b5
JiLao1 <1106751456@qq.com>
2025-12-21 01:01:17 +0800

    1

---
+++ src/config/profileConfig.ts
@@ -0,0 +1,43 @@
+import type { ProfileConfig } from "../types/config";
+
+export const profileConfig: ProfileConfig = {
+	// 头像
+	avatar: "/assets/images/icon.jpg" ,
+	// 名字
+	name: "P_star7",
+	// 个人签名
+	bio: "Hello, I'm P_star7.",
+
+	// 链接配置
+	// 已经预装的图标集：fa6-brands，fa6-regular，fa6-solid，material-symbols，simple-icons
+	// 访问https://icones.js.org/ 获取图标代码，
+	// 如果想使用尚未包含相应的图标集，则需要安装它
+	// `pnpm add @iconify-json/<icon-set-name>`
+	// showName: true 时显示图标和名称，false 时只显示图标
+	links: [
+		{
+			name: "Bilibli",
+			icon: "fa6-brands:bilibili",
+			url: "https://space.bilibili.com/356542846",
+			showName: false,
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/JiLao1",
+			showName: false,
+		},
+		{
+			name: "Email",
+			icon: "fa6-solid:envelope",
+			url: "mailto:1106751456@qq.com",
+			showName: false,
+		},
+		{
+			name: "RSS",
+			icon: "fa6-solid:rss",
+			url: "/rss/",
+			showName: false,
+		},
+	],
+};
---
+++ src/content/posts/my-first-post.md
@@ -0,0 +1,15 @@
+---
+title: 测试文档
+published: 2025-12-20
+pinned: true
+description: 一个简单的Markdown文档测试.
+tags: [Markdown, Blogging]
+category: 测试
+licenseName: "Unlicensed"
+author: P_star7
+draft: false
+date: 2025-12-20
+pubDate: 2025-12-20
+---
+这是本博客第一篇文章
+本文为测试文档
